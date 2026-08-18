d040bbdcfda5b1ad5be2b174c25ee873cb3773dd
Lavanya <lavanyawarang2@gmail.com>
2026-08-18 14:13:51 +0530

    Refactor Tree class and main method formatting
    Refactored Tree class to improve formatting and structure.

--- Syntax.java
+++ Syntax.java
@@ -36,35 +36,35 @@ public static void main (String [] args)
  }
 }
 2.Tree
-     *
-    * *
-   * * *
-  * * * *
-    | |
-    | |
+     
+      *
+    * * *
+  * * * * *
+    | | |
+    
  
-class Tree 
- {
-  public static void main(String [] args)
-  {
-   System.out.print("  ");
-   System.out.print("  ");
-   System.out.print("  ");
-   System.out.println("* ");
-   System.out.print("  ");
-   System.out.print("  ");
-   System.out.print("* ");
-   System.out.println("* ");
-   System.out.print("  ");
-   System.out.print("* ");
-   System.out.print("* ");
-   System.out.println("* ");
-   System.out.print("* ");
-   System.out.print("* ");
-   System.out.print("* ");
-   System.out.println("* ");
-   System.out.print("  ");
-   System.out.print("| ");
-   System.out.println("| ");
-  }
- }
+class Tree
+{
+    public static void main (String [] args)
+    {
+        System.out.print("  ");
+        System.out.print("  ");
+        System.out.println("* ");
+        System.out.print("  ");
+        System.out.print("* ");
+        System.out.print("* ");
+        System.out.println("* ");
+        System.out.print("* ");
+        System.out.print("* ");
+        System.out.print("* ");
+        System.out.print("* ");
+        System.out.println("* ");
+        System.out.print("  ");
+        System.out.print("| ");
+        System.out.print("| ");
+        System.out.println("| ");
+    
+
+    }
+}
+
