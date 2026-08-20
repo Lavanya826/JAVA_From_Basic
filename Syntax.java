d08503763d1f4ca3b8596c852ae222169b6b670b
Lavanya <lavanyawarang2@gmail.com>
2026-08-20 19:32:46 +0530

    Add Differentshape2 class to print shape pattern
    Added a new class 'Differentshape2' that prints a specific shape pattern using asterisks.

--- Syntax.java
+++ Syntax.java
@@ -68,3 +68,41 @@ class Tree
     }
 }
 
+3. Different shape
+ 
+  *   * * * 
+  *   * 
+  * * * * * 
+      *   * 
+  * * *   * 
+
+class Differentshape2
+{
+public static void main(String [] args)
+ {
+ System.out.print("* ");
+ System.out.print("  ");
+ System.out.print("* ");
+ System.out.print("* ");
+ System.out.println("* ");
+ System.out.print("* ");
+ System.out.print("  ");
+ System.out.println("* ");
+ System.out.print("* ");
+ System.out.print("* ");
+ System.out.print("* ");
+ System.out.print("* ");
+ System.out.println("* ");
+ System.out.print("  ");
+ System.out.print("  ");
+ System.out.print("* ");
+ System.out.print("  ");
+ System.out.println("* ");
+ System.out.print("* ");
+ System.out.print("* ");
+ System.out.print("* ");
+ System.out.print("  ");
+ System.out.println("* ");
+ }
+}
+
