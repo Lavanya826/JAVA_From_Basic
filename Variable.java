b8ed2f127f2233e6add67cc997adbc78ac0bfb54
Lavanya <lavanyawarang2@gmail.com>
2026-08-25 23:37:15 +0530

    Create Variable.java

---
+++ Variable.java
@@ -0,0 +1,58 @@
+//Create a Shoe class with 8 variables such as brand, color, size, price, etc. Create an object and print all values.
+class Shoe{
+  public static void main(String [] args){
+ 
+ String brand="puma";
+ System.out.println(brand);
+
+ String color="red";
+ System.out.println(color);
+
+ int size=9;
+ System.out.println(size);
+
+ long price=2000;
+ System.out.println(price);
+ }
+}
+//Create a Car class with 10 variables. Create an object and print all details.
+class Car{
+  public static void main(String [] args){
+    String brandName="BMW";
+    System.out.println(brandName);
+
+    String color="pink";
+    System.out.println(color);
+    
+    String material="Metal";
+    System.out.println(material);
+
+    float rating=5.05f;
+    System.out.println(rating);
+
+    long serialNo=92748237617721832l;
+    System.out.println(serialNo);
+
+    char noPlate=9189;
+    System.out.println(noPlate);
+  }
+}
+
+//Create a Mobile class with variables like brand, model, RAM, storage, price, color, etc. Create an object and print them.
+
+
+
+
+
+//Create a Book class with bookName, author, pages, price, rating, language, etc. Create an object and print all values.
+
+
+
+
+
+//Create a Laptop class with at least 12 variables using different data types (int, double, float, char, boolean, String, etc.).
+
+
+
+
+//Create a Student class with at least 10 variables. Create 2 objects and print their details.
