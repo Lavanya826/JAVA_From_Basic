63770407cd6ce987c85c6ad5a298606aa500e07a
Lavanya <lavanyawarang2@gmail.com>
2026-08-29 00:16:23 +0530

    Document type casting in Type_casting.java
    Added explanations for primitive and non-primitive type casting.

--- Type_casting.java
+++ Type_casting.java
@@ -1,1 +1,12 @@
+There are two types in type casting
+  1.Primitive type casting
+  2.Non-Primitive type casting
+
+In Primitive:
+1.number:byte,short,int,long
+2.character:char
+3.boolean:true or false
+
+In Non-primitive type casting
+1.string
 
