781e49149cbc1c5586be0bf30bbfc847eba40992
Lavanya <lavanyawarang2@gmail.com>
2026-09-03 00:07:11 +0530

    Update Pre-Increment_Post-Increment.java

--- Pre-Increment_Post-Increment.java
+++ Pre-Increment_Post-Increment.java
@@ -1,1 +1,51 @@
+//Java has two increment operators:
+//++a → Pre-increment → first increase, then use the value.
+//a++ → Post-increment → first use the value, then increase it.
 
+//1. Create a program where a variable age starts at 20. Increase the age by 1 before printing it.
+
+class Demo{
+  public static void main(String [] args){ 
+    int age=20;
+    int b = ++ age;
+    System.out.println(b);
+  }
+}
+
+//2.Create a program where a variable count starts at 10. Print its current value and then increase it by 1.
+class Demo{
+  public static void main(String [] args){ 
+    int count=10;
+    int b = ++ count;
+    System.out.println(a);
+    System.out.println(b);
+  }
+}
+
+//3.A student has 5 completed assignments. Increase the number of assignments by 1 before displaying it.
+class Demo{
+  public static void main(String [] args){ 
+    int assignment=5;
+    int b= ++ assignment;
+    System.out.println(b);
+  }
+}
+
+//4.A counter starts at 100. Display the current counter value and then increase it by 1.
+class Demo{
+  public static void main(String [] args){ 
+    int a=100;
+    int b = ++a;
+    System.out.println(a);
+    System.out.println(b);
+  }
+}
+
+//5.A shop has 10 products. Increase the product count by 1 and then display the updated count.
+class Demo{
+  public static void main(String [] args){ 
+    int a=10;
+    int b = ++a;
+    System.out.println(b);
+  }
+}
