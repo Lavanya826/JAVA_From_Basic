7e7ea4cab375a030cab47de24f11fff899ab68c2
Lavanya <lavanyawarang2@gmail.com>
2026-09-04 21:56:25 +0530

    Update Ternary_operator.java

--- Ternary_operator.java
+++ Ternary_operator.java
@@ -1,1 +1,28 @@
+//Example 1.Create class vote and check whether raju is eligible to vote or not if his age=32.
+  class Vote{
+  public static void main (String [] args){
+    int age=32;
+    String res=(age>=18)? "eligible" : "not eligible";
+    System.out.println(res);
+  }
+  }
 
+//Example 2: Check even or odd
+  class Numbers{
+  public static void main(String [] args){
+    int a=-7;
+    String result=(a<=0)? "Its odd number" : "Its even number";
+    System.out .println(result);
+  }
+  }
+ 
+//Example 3: Find greater number
+  class Highestno{
+  public static void main (String [] args){
+    int a=32;
+    int b=90;
+    String res=(a>b)? "a is lower":"b is higher";
+    System.out.println(res);
+  }
+  }
+  
