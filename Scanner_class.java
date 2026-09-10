c4678dd76e70160567b15a9bfcff0a0ba14221c3
Lavanya <lavanyawarang2@gmail.com>
2026-09-10 23:37:36 +0530

    Update Scanner_class.java

--- Scanner_class.java
+++ Scanner_class.java
@@ -37,17 +37,71 @@ class Integer{
     System.out.println("sum");
   }
 }
-4. Take two integers and print their quotient.
-
-5. Take two integers and print the remainder.
-
-6. Take your name as input and print it.
+4. Take two integers and print their quotient. //(USE / FOR GETTING QUOTIENT).
+import java.util.Scanner;
+class Quotient{
+  public static void main(String [] args){
+    Scanner s= new Scanner(System.in);
+    System.out.println("Enter no : ");
+    int a=s.nextInt();
+    System.out.println("Enter no : ");
+    int b=s.nextInt();
+    System.out.println(a/b);
+  }
+}
+5. Take two integers and print the remainder. //(USE % FOR GETTING REMAINDER).
+import java.util.Scanner;
+class Remainder{
+  publis staic void main(String [] args){
+    Scanner s= new Scanner(System.in);
+    System.out.println("Enter no : ");
+    int a=s.nextInt();
+    System.out.println("Enter no : ");
+    int b=s.nextInt();
+    System.out.println(a%b);
+  }
+}
+6. Take your name as input and print it. //(WHILE USING STRING JUST USE [String=s.next()] AND THE WORD STRING SHOULD BE CAPITAL)
+import java.util.Scanner;
+class Input{
+  public static void main(String [] args){
+    Scanner s= new Scanner(System.in);
+    System.out.println("Enter your name : ");
+    String a=s.next(); //THIS ONE
+    System.out.println(a);
+  }
+}
 
 7. Take your age as input and print it.
-
+import java.util.Scanner;
+class Input{
+  public static void main(String [] args){
+    Scanner s= new Scanner(System.in);
+    System.out.print("Enter your age : ");
+    int a=s.nextInt(); 
+    System.out.println(a);
+  }
+}
 8. Take a decimal number as input and print it.
-
+import java.util.Scanner;
+class Input{
+  public static void main(String [] args){
+    Scanner s= new Scanner(System.in);
+    System.out.print("Enter number : ");
+    decimal a=s.nextDecimal();    //SO HERE WHILE TAKING THE DECIMAL WE HAVE TO TAKE DOUBLE 
+    System.out.println(a);
+  }
+}
 9. Take a character as input and print it.
+import java.util.Scanner;
+class Input{
+  public static void main(String [] args){
+    Scanner s= new Scanner(System.in);
+    System.out.print("Enter Character : ");
+    char a=s.next().charAt(0);     //THIS ONE REMEMBER ,EXTRA WE HAVE TO TAKE charAt(0)
+    System.out.println(a);
+  }
+}
 
 10. Take student name, age, and marks as input and print all three.
 
