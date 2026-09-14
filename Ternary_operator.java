ad80a629ba3f5c31bf2c4792a64f840306bde6da
Lavanya <lavanyawarang2@gmail.com>
2026-09-14 21:03:12 +0530

    Update Ternary_operator.java

--- Ternary_operator.java
+++ Ternary_operator.java
@@ -25,4 +25,52 @@
     System.out.println(res);
   }
   }
-  
+
+
+
+//SACNNER CLASS USING TERNARY OPERATOR
+//check whether triangle is right angled, obtuse angled or acute angled.
+import java.util.Scanner;
+class Demo{
+  public static void main(String [] args){
+    Scanner s= new Scanner(System.in);
+    System.out.print("Enter all 3 sides : ");
+    int a=s.nextInt();
+    int b=s.nextInt();
+    int c=s.nextInt();
+    String res=(c*c=a*a+b*b)?"RIGHT ANGLE": //3 4 5 
+               (c*c>a*a+b*b)?"OBTUSE ANGLE": //3 4 6
+               "ACUTE ANGLE";
+    System.out.println(res);
+  }
+}
+
+//Cheack whether triangle is Equilateral, Isosceles or Scalene.
+import java.util.Scanner;
+class Demo{
+  public static void main(String [] args){
+    Scanner s= new Scanner(System.in);
+    System.out.print("Enter all 3 sides : ");
+    int a=s.nextInt();
+    int b=s.nextInt();
+    int c=s.nextInt();
+    String res=(a==b && b==c)?"EQUILATERAL": //3 4 5 
+               (a==b || b==c || c==a)?"ISOSALES": //3 4 6
+               "SCALAR";
+    System.out.println(res);
+  }
+}
+
+//Cheack whether triangle is Positive,Negative and Neutral.
+import java.util.Scanner;
+class Demo{
+  public static void main(String [] args){
+    Scanner s= new Scanner(System.in);
+    System.out.print("Enter all 3 sides : ");
+    int a=s.nextInt();
+    String res=(a<0)?("negative"):
+               (a>0)?("positive"):
+                ("neutral");
+    System.out.println(res);
+  }
+}
