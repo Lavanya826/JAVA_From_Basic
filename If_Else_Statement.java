34423388dcc9a7654472e6863f04afd4b57f1b9e
Lavanya <lavanyawarang2@gmail.com>
2026-09-16 19:13:08 +0530

    Document If-Else statement in If_Else_Statement.java
    Added explanation for If-Else statement in Java.

---
+++ If_Else_Statement.java
@@ -0,0 +1,3 @@
+                                                      //IF_ELSE_STATEMENT
+If Else statement is a conditional statment in java which is use to help the programmer to make the decision whether to execute a block of 
+code or not based on certain condition.  
