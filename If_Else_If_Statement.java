024a8d26466e08d0622f572e37474cdc303fe482
Lavanya <lavanyawarang2@gmail.com>
2026-09-17 23:58:03 +0530

    Update If_Else_If_Statement.java

--- If_Else_If_Statement.java
+++ If_Else_If_Statement.java
@@ -1,1 +1,17 @@
-
+class Demo{
+  public static void main(String [] args){
+    int temp=35;
+    if(temp >40){
+      System.out.println("Its very hot");
+    }
+    else if(temp==30){
+      System.out.println("Its hot")
+    }
+    else if(temp==29){
+      System.out.println("Its Normal")
+    }
+    else{
+      System.out.println("Its cold");
+    }
+  }
+}
