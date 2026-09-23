c379527d6c73496ce860550efcd3a79ddafb0b86
Lavanya <lavanyawarang2@gmail.com>
2026-09-23 23:53:54 +0530

    Update For_Loop_Statement.java

--- For_Loop_Statement.java
+++ For_Loop_Statement.java
@@ -1,1 +1,55 @@
+                          //FOR LOOP STATEMENT
 
+For loop is used when we want to repeat a block of code multiple times.
+For example while printing pattern...we were doing this...System.out.println(" * ")...but now if there are 5000 lines then we wont type
+  the line one by one instead we will use for loop.
+SYNTAX:
+  for( iteration ; condition ; updation){
+    //statement
+  }
+PROGRAM:
+Level 1 — Basic
+  
+1. Print numbers from 1 to 10.
+class Demo{
+  public static void main(String [] args){
+    for( int a=1; a<=10; a++){
+      System.out.println(a);
+    }
+  }
+}
+
+2. Print numbers from 10 to 1.
+class Demo{
+  public static void main(String [] args){
+    for(int b=10;b>=1 ;b--){
+      System.out.println(b);
+    }
+  }
+}
+3. Print numbers from 1 to 20.
+class Demo{
+  public static void main(String [] args){
+    for(int c=1;c<=20 ;c++){
+      System.out.println(c);
+    }
+  }
+}
+
+4. Print all even numbers from 1 to 20.
+class Demo{
+  public static void main(String [] args){
+    for(int c=2;c<=20 ;c+=2){
+      System.out.println(c);
+    }
+  }
+}
+
+5. Print all odd numbers from 1 to 20.
+class Demo{
+  public static void main(String [] args){
+    for(int c=1;c<=20 ;c+=2){
+      System.out.println(c);
+    }
+  }
+}
