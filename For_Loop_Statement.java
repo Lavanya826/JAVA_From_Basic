0c4635b1afac5d917ae65919e20f0bde8e86f668
Lavanya <lavanyawarang2@gmail.com>
2026-09-25 00:07:45 +0530

    Update For_Loop_Statement.java

--- For_Loop_Statement.java
+++ For_Loop_Statement.java
@@ -53,3 +53,20 @@ class Demo{
     }
   }
 }
+
+6.Print even number 10 to 1
+import java.util.Scanner;
+class Even{
+  public static void main(String [] args){
+    Scanner s=new Scanner(System.in);
+    System.out.println("Enter sp :");
+    int sp=s.nextInt();
+    System.out.println("Enter ep :");
+    int ep=s.nextInt();
+    for(int i=10;i>=1;i--){
+      if(i%2==0){
+        System.out.println(i);
+      }
+    }
+  }
+}
